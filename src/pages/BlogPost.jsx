1541411c2932ff45e847617c86c526f04f576e4e
benkaz3 <alikojro@gmail.com>
2024-11-04 16:39:10 +0700

    Resvised formatting of richText for blog posts

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -20,7 +20,35 @@ const renderRichText = (richTextNode) => {
             {renderRichText(node)}
           </p>
         );
+      case 'heading-1':
+        return (
+          <h1 key={index} className='text-3xl font-bold mb-4'>
+            {renderRichText(node)}
+          </h1>
+        );
+      case 'heading-2':
+        return (
+          <h2 key={index} className='text-2xl font-semibold mb-3'>
+            {renderRichText(node)}
+          </h2>
+        );
+      case 'heading-3':
+        return (
+          <h3 key={index} className='text-xl font-semibold mb-2'>
+            {renderRichText(node)}
+          </h3>
+        );
       case 'text':
+        const hasCodeMark = node.marks && node.marks.some(mark => mark.type === 'code');
+        if (hasCodeMark) {
+          return (
+            <div className='p-4 border-0 border-gray-300 bg-gray-100 rounded'>
+              <code key={index} className='text-gray-800 px-2 rounded text-sm font-mono'>
+                {node.value}
+              </code>
+            </div>
+          );
+        }
         return node.value;
       case 'hyperlink':
         return (
@@ -32,25 +60,43 @@ const renderRichText = (richTextNode) => {
             {node.content.map((linkNode) => linkNode.value).join('')}
           </a>
         );
-      case 'ol': // Ordered list
-        return (
-          <ol key={index} className='list-decimal list-inside mb-4'>
-            {node.content.map((listItem, itemIndex) => (
-              <li key={itemIndex}>{renderRichText(listItem)}</li>
-            ))}
-          </ol>
-        );
-      case 'ul': // Unordered list
+        case 'ordered-list':
+  return (
+    <ol key={index} className='list-decimal list-inside mb-4'>
+      {node.content.map((listItem, itemIndex) => (
+        <li key={itemIndex} className='mb-2 flex items-start'>
+          {renderRichText(listItem)}
+        </li>
+      ))}
+    </ol>
+  );
+
+
+
+
+        
+      case 'unordered-list':
         return (
           <ul key={index} className='list-disc list-inside mb-4'>
             {node.content.map((listItem, itemIndex) => (
-              <li key={itemIndex}>{renderRichText(listItem)}</li>
+              <li key={itemIndex} className='mb-2 inline'> {/* Use 'inline' to keep items on the same line */}
+                {renderRichText(listItem)}
+              </li>
             ))}
           </ul>
         );
-      case 'hr': // Horizontal rule
+      case 'blockquote':
+        return (
+          <blockquote
+            key={index}
+            className='border-l-4 border-gray-300 pl-4 italic text-gray-700 mb-4'
+          >
+            {renderRichText(node)}
+          </blockquote>
+        );
+      case 'hr':
         return <hr key={index} className='my-4 border-t border-gray-300' />;
-      case 'embedded-asset': // Embedded asset (like images)
+      case 'embedded-asset-block':
         const { title, file } = node.data.target.fields;
         return (
           <div key={index} className='my-4'>
@@ -62,20 +108,64 @@ const renderRichText = (richTextNode) => {
             {title && <p className='text-center text-sm text-gray-600'>{title}</p>}
           </div>
         );
-      case 'embedded-entry-block': // Embedded entry
-        const entry = node.data.target.fields;
+
+        case 'embedded-entry-block':
+          const entryBlock = node.data.target.fields;
+          return (
+            <div key={index} className='my-4'>
+              <a 
+                  href={`/vi/blog/${entryBlock.slug}`}
+
+              >
+                <div className='flex items-center p-4 bg-gray-200 border rounded-sm hover:bg-section_background transition'>
+                  <h3 className='text-lg font-bold mb-2'>
+                    {entryBlock.title}
+                  </h3>
+                  {entryBlock.image && entryBlock.image.fields.file && (
+                  <img
+                    src={entryBlock.image.fields.file.url}
+                    alt={entryBlock.title}
+                    className='w-16 h-16 object-cover rounded ml-4'
+                  />
+                )}
+                  {entryBlock.introduction && (
+                    <p className='text-gray-700'>{entryBlock.introduction.slice(0, 100)}...</p>
+                  )}
+                </div>
+              </a>
+            </div>
+          );
+        
+      case 'embedded-entry-inline':
+        const inlineEntry = node.data.target.fields;
         return (
-          <div key={index} className='my-4'>
-            <h3 className='text-xl font-bold'>{entry.title}</h3>
-            <p>{renderRichText(entry.body)}</p>
-          </div>
+          <a
+            key={index}
+            href={`/vi/luat-su/${inlineEntry.slug}`}
+            className='text-blue-600 hover:underline inline-block'
+          >
+            {inlineEntry.name || inlineEntry.title}
+          </a>
         );
-      // Add more cases as necessary for other types of nodes
+      case 'code':
+        return (
+          <pre key={index} className='bg-gray-800 text-sm text-white p-4 rounded-lg overflow-x-auto'>
+            <code>{node.content[0].value}</code>
+          </pre>
+        );
       default:
+        console.warn(`Unsupported node type: ${node.nodeType}`);
         return null;
     }
   });
 };
+
+
+
+
+
+
+
 
 const BlogPost = () => {
   const { t } = useTranslation(); // Get the t function for translation
@@ -104,6 +194,7 @@ const BlogPost = () => {
   }
 
   const post = data.blogPage ? data.blogPage[0] : null;
+  // console.log(`data: ${JSON.stringify(data.blogPage)}`);
 
   if (!post) {
     return (
