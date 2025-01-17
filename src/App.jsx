112461e5bc854e565b71e5b655f1316618c9016d
benkaz3 <alikojro@gmail.com>
2025-01-17 18:51:21 +0700

    Refined app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
@@ -19,61 +15,43 @@ import BlogCategoryPage from './pages/BlogCategoryPage';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-AOS.init({
-  duration: 500, // Duration of animations
-  once: false, // Whether animation should happen only once while scrolling down
-});
-
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 500,
+      once: false,
+    });
+  }, []);
+
   return (
-    // <LanguageProvider>
     <Router>
       <Routes>
         <Route element={<Layout />}>
-          {/* Redirect `/` to `/vi` */}
-          <Route path='/' element={<Navigate replace to='/vi' />} />
-          <Route path='/en' element={<HomePage />} />
-          <Route path='/vi' element={<HomePage />} />
-          <Route
-            path='/vi/linh-vuc-va-nganh-nghe/:slug'
-            element={<PracticeDetailsPage />}
-          />
-          <Route
-            path='/en/practices-and-sectors/:slug'
-            element={<PracticeDetailsPage />}
-          />
-          <Route
-            path='/en/practices-and-sectors/'
-            element={<PracticesAndSectorsSection />}
-          />
-          <Route
-            path='/vi/linh-vuc-va-nganh-nghe'
-            element={<PracticesAndSectorsSection />}
-          />
-          <Route path='/vi/lien-he' element={<ContactPage />} />
-          <Route path='/en/contact' element={<ContactPage />} />
-          <Route path='/en/blog' element={<BlogPage />} />
-          <Route path='/vi/blog' element={<BlogPage />} />
-          <Route path='/vi/blog/:slug' element={<BlogPost />} />
-          <Route path='/en/blog/:slug' element={<BlogPost />} />
-          <Route path='/en/attorneys' element={<OurTeamPage />} />
-          <Route path='/vi/luat-su' element={<OurTeamPage />} />
-          <Route path='/vi/luat-su/:slug' element={<AuthorProfile />} />
-          <Route path='/en/attorneys/:slug' element={<AuthorProfile />} />
-          <Route path='/en/policies' element={<PoliciesPage />} />
-          <Route path='/vi/chinh-sach' element={<PoliciesPage />} />
-          <Route path='/vi/blog/chu-de/:slug' element={<BlogCategoryPage />} />
-          {/* <Route path='/category/:slug' element={<BlogCategoryPage />} /> */}
-
-          <Route
-            path='/en/blog/category/:slug'
-            element={<BlogCategoryPage />}
-          />
-          <Route path='*' element={<NotFoundPage />} />
+          <Route path="/" element={<Navigate replace to="/vi" />} />
+          <Route path="/en" element={<HomePage />} />
+          <Route path="/vi" element={<HomePage />} />
+          <Route path="/vi/linh-vuc-va-nganh-nghe/:slug" element={<PracticeDetailsPage />} />
+          <Route path="/en/practices-and-sectors/:slug" element={<PracticeDetailsPage />} />
+          <Route path="/en/practices-and-sectors" element={<PracticesAndSectorsSection />} />
+          <Route path="/vi/linh-vuc-va-nganh-nghe" element={<PracticesAndSectorsSection />} />
+          <Route path="/vi/lien-he" element={<ContactPage />} />
+          <Route path="/en/contact" element={<ContactPage />} />
+          <Route path="/en/blog" element={<BlogPage />} />
+          <Route path="/vi/blog" element={<BlogPage />} />
+          <Route path="/vi/blog/:slug" element={<BlogPost />} />
+          <Route path="/en/blog/:slug" element={<BlogPost />} />
+          <Route path="/en/attorneys" element={<OurTeamPage />} />
+          <Route path="/vi/luat-su" element={<OurTeamPage />} />
+          <Route path="/vi/luat-su/:slug" element={<AuthorProfile />} />
+          <Route path="/en/attorneys/:slug" element={<AuthorProfile />} />
+          <Route path="/en/policies" element={<PoliciesPage />} />
+          <Route path="/vi/chinh-sach" element={<PoliciesPage />} />
+          <Route path="/vi/blog/chu-de/:slug" element={<BlogCategoryPage />} />
+          <Route path="/en/blog/category/:slug" element={<BlogCategoryPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </Router>
-    // </LanguageProvider>
   );
 }
 
